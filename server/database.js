de926f6d87dc650c7ac1863db7b95db72a6ad5c0
César Ortega <ortegacesar06@gmail.com>
2019-02-26 10:16:52 -0500

    .

--- server/database.js
+++ server/database.js
@@ -1,8 +1,8 @@
 'use strict';
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('postgres', 'postgres', 'root', {
-    host: 'localhost',
+const sequelize = new Sequelize('dapsk9p2hue5a5', 'glmhrflygedhgv', '70d97e870ef1715bc6f954b60568d03f15a8df3390481c3bf16baceb63bc6554', {
+    host: 'ec2-107-20-185-27.compute-1.amazonaws.com',
     dialect: 'postgres',
     port: 5432,
     operatorsAliases: false
